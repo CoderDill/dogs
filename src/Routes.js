c7ce6940531c83e8c901fb1adaaab2efe4b5d9c3
Matthew Dillon <mddillon47@gmail.com>
2021-12-25 19:38:30 -0800

    Refactored with Redirect

--- src/Routes.js
+++ src/Routes.js
@@ -1,26 +1,21 @@
+import { Switch, Route, Redirect } from "react-router-dom";
+import DogList from "./DogList";
+import DogDetail from "./DogDetail";
 
-import { Switch, Route } from 'react-router-dom'
-import DogList from './DogList'
-import DogDetail from './DogDetail'
-
-
-
-function Routes({dogs}) {
-    return (
-        <>
-            <Switch>
-                <Route exact path="/dogs">
-                    <DogList dogs={dogs}/>
-                </Route>
-                <Route exact path="/dogs/:name">
-                    <DogDetail dogs={dogs}/>
-                </Route>
-                <Route>
-                    <DogList dogs={dogs}/>
-                </Route>
-            </Switch>
-        </>
-    )
+function Routes({ dogs }) {
+  return (
+    <>
+      <Switch>
+        <Route exact path="/dogs">
+          <DogList dogs={dogs} />
+        </Route>
+        <Route exact path="/dogs/:name">
+          <DogDetail dogs={dogs} />
+        </Route>
+        <Redirect to="/dogs" />
+      </Switch>
+    </>
+  );
 }
 
-export default Routes
+export default Routes;
